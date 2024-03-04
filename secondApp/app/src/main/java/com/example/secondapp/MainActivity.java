85d2e469980c4ecddcbb61506296a1e9b60d3731
superillegal <r.rogah@mail.ru>
2024-03-04 09:34:14 +0300

    init

---
+++ secondApp/app/src/main/java/com/example/secondapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.secondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.os.Build;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("RRR","onCreate()");
+        setContentView(R.layout.activity_main);
+
+        String androidVersion = getString(R.string.version, Build.VERSION.SDK_INT);
+        TextView textViewVersion = findViewById(R.id.version);
+        textViewVersion.setText(androidVersion);
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("SET", "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("SET", "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("SET", "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("SET", "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("SET", "onRestart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("SET", "onDestroy: ");
+    }
+
+}
---
+++ secondAppe/app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = findViewById(R.id.name);
+                String textToSend = editText.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                intent.putExtra("textFromMain", textToSend);
+                startActivity(intent);
+            }
+        });
+    }
+}
